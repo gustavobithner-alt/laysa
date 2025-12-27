d4f733fc9c1beb548bc7601176e5a7e5df6ab2f2
gustavobithner-alt <gustavobithner@gmail.com>
2025-12-27 16:47:13 -0300

    Primeira atualização

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+  const texto = document.getElementById("texto");
+  texto.innerText = "Eu fiz esse site pensando em você, Laysa 💙";
+}
+
+function toggleItem(elemento) {
+  elemento.classList.toggle("ativo");
+}
